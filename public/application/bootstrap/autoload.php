9f9a3c10ad326799ad54f8b865d0555e6377b6d9
Korvin Szanto <Korvinszanto@gmail.com>
2017-05-11 15:04:17 -0700

    Make sure we set the include path properly

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -5,5 +5,5 @@ defined('C5_EXECUTE') or die('Access Denied.');
 require_once __DIR__ . "/../../../vendor/autoload.php";
 
 # Add the vendor directory to the include path
-ini_set('include_path', __DIR__ . "/../../vendor" . PATH_SEPARATOR . get_include_path());
+ini_set('include_path', __DIR__ . "/../../../vendor" . PATH_SEPARATOR . get_include_path());
 
