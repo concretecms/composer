e425834813a4e5241c70d11c20b6b782b7121fff
Korvin Szanto <korvinszanto@gmail.com>
2017-12-28 12:11:07 -0800

    Add default dotenv setup

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -4,6 +4,15 @@ defined('C5_EXECUTE') or die('Access Denied.');
 # Load in the composer vendor files
 require_once __DIR__ . "/../../../vendor/autoload.php";
 
+# Try loading in environment info
+$env = new \Dotenv\Dotenv(__DIR__ . '/../../../');
+try {
+    $env->overload();
+} catch (\Exception $e) {
+    // Ignore any errors
+    dd($e);
+}
+
 # Add the vendor directory to the include path
 ini_set('include_path', __DIR__ . "/../../../vendor" . PATH_SEPARATOR . get_include_path());
 
