810ad27cf951267bfff52326cf77d8a31684ee08
Korvin Szanto <korvinszanto@gmail.com>
2017-12-29 10:14:38 -0800

    Add sample live.database.php file

---
+++ public/application/config/live.database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'default-connection' => 'concrete',
+    'connections' => [
+        'concrete' => [
+            'driver' => 'c5_pdo_mysql',
+            'server' => getenv('DB_HOSTNAME'),
+            'database' => getenv('DB_DATABASE'),
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset' => getenv('DB_CHARSET'),
+        ],
+    ],
+];
