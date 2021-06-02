d99fe04b4171d5141322edfb842fec8be3cedefb
Korvin Szanto <korvin@portlandlabs.com>
2021-06-02 13:03:16 -0700

    Add configuration to hide DB_PASSWORD and DB_HOSTNAME in error output
    I opened a PR [here](https://github.com/concrete5/concrete5/pull/9506) to add support for this config setting, let's set it by default in anticipation of it becoming supported soon.

--- public/application/config/concrete.php
+++ public/application/config/concrete.php
@@ -5,4 +5,10 @@ return [
         // Skip the automatic check of new concrete5 versions availability
         'skip_core' => true,
     ],
+    'debug' => [
+        'hide_keys' => [
+            // Hide database password and hostname in whoops output if supported
+            '_ENV' => ['DB_PASSWORD', 'DB_HOSTNAME'],
+        ]
+    ]
 ];
