0aca2b591e00cc4052486b6a88df7ddb058a23ad
Korvin Szanto <korvin@portlandlabs.com>
2024-03-05 10:58:25 -0800

    Use  instead of  so live.database.php doesn't override install by default

--- public/application/bootstrap/autoload.php
+++ public/application/bootstrap/autoload.php
@@ -7,10 +7,11 @@ require_once __DIR__ . "/../../../vendor/autoload.php";
 # Try loading in environment info
 try {
     (new \Symfony\Component\Dotenv\Dotenv('CONCRETE5_ENV'))
-        ->usePutenv()->loadEnv(__DIR__ . '/../../../.env');
+        ->usePutenv()->load(__DIR__ . '/../../../.env');
 } catch (\Symfony\Component\Dotenv\Exception\PathException $e) {
     // Ignore missing file exception
 }
 
 # Add the vendor directory to the include path
 ini_set('include_path', __DIR__ . "/../../../vendor" . PATH_SEPARATOR . get_include_path());
+
